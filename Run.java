b7e01e120569166fc719390418bfb53e2a59a87c
wardha <wardha.21@gmail.com>
2014-12-30 15:43:11 +0500

    Chapter 7

--- Run.java
+++ Run.java
@@ -37,20 +37,27 @@ public class Run {
 		 assertEquals(new Dollar(15), five.times(3));
 		}*/
 
-	
+	/*
 	  public void testFrancMultiplication() {
 		 Franc five= new Franc(5);
 		 assertEquals(new Franc(10), five.times(2));
 		 assertEquals(new Franc(15), five.times(3));
 		}
 	
-	  @Test public void testEqualityChapter6() {
+	   public void testEqualityChapter6() {
 		 assertTrue(new Dollar(5).equals(new Dollar(5)));
 		 assertFalse(new Dollar(5).equals(new Dollar(6)));
 		 assertTrue(new Franc(5).equals(new Franc(5)));
 		 assertFalse(new Franc(5).equals(new Franc(6)));
-		}
+		}*/
 
+	   @Test public void testEquality() {
+		  assertTrue(new Dollar(5).equals(new Dollar(5)));
+		  assertFalse(new Dollar(5).equals(new Dollar(6)));
+		  assertTrue(new Franc(5).equals(new Franc(5)));
+		  assertFalse(new Franc(5).equals(new Franc(6)));
+		  assertFalse(new Franc(5).equals(new Dollar(5)));
+		 }
 
 
 }
