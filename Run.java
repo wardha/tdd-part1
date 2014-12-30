df145163c7aac3cc8e58a9cd4ad35bbcb63e0985
wardha <wardha.21@gmail.com>
2014-12-30 13:04:11 +0500

    Chapter 3

--- Run.java
+++ Run.java
@@ -1,5 +1,7 @@
 package TDDPart1Classes;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
@@ -15,12 +17,18 @@ public class Run {
 		
 	}
 	
-	@Test public void testMultiplication() {
+	public void testMultiplication() {
 		 Dollar five= new Dollar(5);
 		 Dollar product= five.times(2);
 		 assertEquals(10, product.amount);
 		 product= five.times(3);
 		 assertEquals(15, product.amount);
+		}
+
+	
+	@Test  public void testEquality() {
+		 assertTrue(new Dollar(5).equals(new Dollar(5)));
+		 assertFalse(new Dollar(5).equals(new Dollar(6)));
 		}
 
 }
