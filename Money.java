b8329d26ba663fefc91b937dbc394c103bef490a
wardha <wardha.21@gmail.com>
2014-12-31 11:03:45 +0500

    Chapter 8

--- Money.java
+++ Money.java
@@ -1,8 +1,17 @@
 package TDDPart1Classes;
 
-public class Money {
+public abstract class Money {
 	protected int amount;
 
+	abstract Money times(int multiplier);
+	
+	static Money dollar(int amount) {
+		return new Dollar(amount);
+	}
+	static Money franc(int amount) {
+		return new Franc(amount);
+	}
+	
 	public boolean equals(Object object) {
 		 Money money = (Money) object;
 		 return amount == money.amount
