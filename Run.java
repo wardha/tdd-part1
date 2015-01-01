50fe4df1fd43d86581913640115505c9f3c44fc1
wardha <wardha.21@gmail.com>
2015-01-01 13:01:26 +0500

    Chapter 14

--- Run.java
+++ Run.java
@@ -102,7 +102,7 @@ public class Run {
 		}
 		*/
 	
-	@Test public void testPlusReturnsSum() {
+/*	@Test public void testPlusReturnsSum() {
 		Money five= Money.dollar(5);
 		Expression result= five.plus(five);
 		Sum sum= (Sum) result;
@@ -121,6 +121,20 @@ public class Run {
 		Bank bank= new Bank();
 		Money result= bank.reduce(Money.dollar(1), "USD");
 		assertEquals(Money.dollar(1), result);
+		}*/
+	@Test public void testReduceMoneyDifferentCurrency() {
+		Bank bank= new Bank();
+		bank.addRate("CHF", "USD", 2);
+		Money result= bank.reduce(Money.franc(2), "USD");
+		assertEquals(Money.dollar(1), result);
+		}
+	
+	@Test public void testArrayEquals() {
+		assertEquals(new Object[] {"abc"}, new Object[] {"abc"});
+		}
+	
+	@Test public void testIdentityRate() {
+		assertEquals(1, new Bank().rate("USD", "USD"));
 		}
 
 	
--- Sum.java
+++ Sum.java
@@ -3,14 +3,14 @@ package TDDPart1Classes;
 public class Sum implements Expression{
 	Money augend;
 	Money addend;
-	
+
 	Sum(Money augend, Money addend) {
 		this.augend= augend;
 		this.addend= addend;
-		}
-	
-	public Money reduce(String to) {
+	}
+
+	public Money reduce(Bank bank, String to) {
 		int amount= augend.amount + addend.amount;
 		return new Money(amount, to);
-		}
+	}
 }
